18d9083d2480ac68794c7a0d068a8636f520bd0f
Antony <antonykleverton.guedes@estudante.ufjf.br>
2025-09-23 22:12:52 -0300

    adição do componente Disciplina e implementação do mesmo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import './App.css';
+import Disciplina from './Disciplina';
 function App() {
   return (
     <>
       <h1>Hello world! 🚧</h1>
       <h2>How are you?</h2>
+      <Disciplina/>
     </>
   )
 
