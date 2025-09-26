9e93f45d2f648d021840b917a11a44a87cf8abb9
antony <antinykleverton.guedes@estudante.ufjf.br>
2025-09-25 22:13:24 -0300

    :label: adicionando tipos com props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import './App.css';
 import Disciplina from './Disciplina';
 function App() {
   return (
     <>
       <h1>Hello world! 🚧</h1>
       <h2>How are you?</h2>
-      <Disciplina/>
+      <Disciplina codigo="DCC206" nome= "Desenvolvimento Web 207"/>
     </>
-  )
-
+  );
 }
-
+ 
 export default App
